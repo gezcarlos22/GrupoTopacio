5ea78fa34f2f915d0139d05ffa980cc4189a3f33
gezcarlos22 <ceg290698@gmail.com>
2025-09-30 21:52:05 -0300

    Comienzo del Proyecto

---
+++ app/src/main/java/com/example/grupotopacio/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.grupotopacio
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val btnLogoButton = findViewById<ImageButton>(R.id.btnLogoButton)
+        btnLogoButton.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
